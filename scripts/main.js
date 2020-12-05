51331852294dee418d8df50b0035867d3d48a180
Piotr Rogawski <rogawski.piotr@gmail.com>
2020-12-05 20:13:16 +0100

    Initialize

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    // Hamburger effect and menu slider
+
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function() {
+        $hamburger.toggleClass("is-active"); 
+        $("#menu").slideToggle();
+    });
+
+    $(window).resize(function() {
+        if($(window).width() >= 768) {
+            $("#menu").css("display", "");
+            $hamburger.removeClass("is-active"); 
+        }
+    });
+    
+});
+
+
+

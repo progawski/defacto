ebf90538fd2f342facbf4ecd3daeedbb4cfe7b46
Piotr Rogawski <rogawski.piotr@gmail.com>
2021-02-11 23:08:04 +0100

    Added carousel repsonsivity and two way rotation

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    
+ 
+
     // Hamburger effect and menu slider
 
     var $hamburger = $(".hamburger");
@@ -8,12 +11,18 @@ $(document).ready(function(){
         $("#menu").slideToggle();
     });
 
+    var smallCarousel = false;
+
     $(window).resize(function() {
         if($(window).width() >= 768) {
             $("#menu").css("display", "");
             $hamburger.removeClass("is-active"); 
+            smallCarousel = false;
         }
     });
+
+    $('.certificate-container:first .certificate').addClass('scale-50');
+    $('.certificate-container:nth(2) .certificate').addClass('scale-50');
     
     // Smooth auto-scrolling
 
@@ -43,21 +52,14 @@ $(document).ready(function(){
     
     //Carousel
 
-    
-
-   
-
     var numCertificates = $('.certificate').length;
     var currentCertificate = 1;
 
-    console.log(numCertificates);
-
     $('#certificates-counter').text(currentCertificate + '/' + numCertificates);
-    $('.certificate-container:first .certificate').addClass('scale-50');
-    $('.certificate-container:nth(2) .certificate').addClass('scale-50');
 
 
     $('#btn-left').on('click', rotateLeft);
+    $('#btn-right').on('click', rotateRight);
 
 
     function rotateLeft(){
@@ -75,54 +77,63 @@ $(document).ready(function(){
         var lastCertificate = $('#carousel-container').find('.certificate-container:last');
         var width = firstCertificate.outerWidth();
 
-
-        secondCertificate.fadeTo(500, 0.5);
-        thirdCertificate.fadeTo(500, 1);
-
         firstCertificate.children().removeClass('scale-50 scale-100').addClass('scale-0');
         secondCertificate.children().removeClass('scale-0 scale-100').addClass('scale-50');
         thirdCertificate.children().removeClass('scale-0 scale-50').addClass('scale-100');
         lastCertificate.children().removeClass('scale-0 scale-100').addClass('scale-50');
 
-        // firstCertificate.children().animate({
-        //     height: '20%'
-        // }, {duration: 500, queue: false});
-        // secondCertificate.children().animate({
-        //     height: '50%'
-        // }, {duration: 500, queue: false});
-        // lastCertificate.children().animate({
-        //     height: '20%',
-        // }, {duration: 500, queue: false});
-        // thirdCertificate.children().animate({
-        //     height: '100%',
-        // }, {duration: 500, queue: false});
+
         firstCertificate.animate({
             marginLeft: -width
         }, {duration: 500, queue: false, complete: function(){
             lastCertificate.after(firstCertificate);
-            // firstCertificate.children().css('width', '100%');
             $('#btn-left').on('click', rotateLeft);
             firstCertificate.css({
                 marginLeft: '0',
             });
         }});
         
-        // secondCertificate.children().animate({
-        //     width: '40%'
-        // }, {duration: 500, queue: false});
-        // thirdCertificate.children().animate({
-        //     width: '100%'
-        // }, {duration: 500, queue: false, complete: function(){
-            
-        
-            // firstCertificate.children().css({
-            //     width: '40%'
-            // });
-        // }});
-        
-        
-
     }
+    function rotateRight(){
+        $('#btn-right').off('click');
+        currentCertificate--;
+        if(currentCertificate < 1){
+            currentCertificate = 4;
+        }
+
+        $('#certificates-counter').text(currentCertificate + '/' + numCertificates);
+
+      
+        var firstCertificate = $('#carousel-container').find('.certificate-container:nth(2)');
+        var secondCertificate = $('#carousel-container').find('.certificate-container:nth(1)');
+        var thirdCertificate = $('#carousel-container').find('.certificate-container:first');
+        var lastCertificate = $('#carousel-container').find('.certificate-container:last');
+       
+        var width = firstCertificate.outerWidth();
+
+        thirdCertificate.before(lastCertificate);
+        lastCertificate.css({marginLeft: -width});
+
+        firstCertificate.children().removeClass('scale-50 scale-100').addClass('scale-0');
+        secondCertificate.children().removeClass('scale-0 scale-100').addClass('scale-50');
+        thirdCertificate.children().removeClass('scale-0 scale-50').addClass('scale-100');
+        lastCertificate.children().removeClass('scale-0 scale-100').addClass('scale-50');
+
+
+
+        lastCertificate.animate({
+            
+            marginLeft: 0
+        }, {duration: 500, queue: false, complete: function(){
+           
+            $('#btn-right').on('click', rotateRight);
+      
+            
+        }});
+        
+    }
+
+
     // $('#btn-right').on('click', function(){
     //     currentCertificate--;
     //     if(currentCertificate < 1){
